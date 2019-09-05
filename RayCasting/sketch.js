3dc5c8f5597bb0af157cc2682b92b049beabe01d
churmd <churmdaniel@gmail.com>
2019-09-05 21:35:03 +0100

    Added random walls

--- RayCasting/sketch.js
+++ RayCasting/sketch.js
@@ -8,16 +8,25 @@ function setup() {
   var cnv = createCanvas(windowWidth, windowHeight);
   cnv.style('display', 'block');
 
+  light = new LightSource(windowWidth/2, windowHeight/2);
+
   var topLeft = createVector(0, 0);
   var topRight = createVector(windowWidth-1, 0);
   var bottomLeft = createVector(0, windowHeight-2);
   var bottomRight = createVector(windowWidth-1, windowHeight-2);
 
-  light = new LightSource(windowWidth/2, windowHeight/2);
   walls.push(new LineSegment(topLeft, topRight));
   walls.push(new LineSegment(topRight, bottomRight));
   walls.push(new LineSegment(bottomRight, bottomLeft));
   walls.push(new LineSegment(bottomLeft, topLeft));
+
+  for(var i = 0; i < 10; i++) {
+    var x1 = random(windowWidth);
+    var y1 = random(windowHeight);
+    var x2 = random(windowWidth);
+    var y2 = random(windowHeight);
+    walls.push(new LineSegment(createVector(x1, y1), createVector(x2, y2)));
+  }
 }
 
 function draw() {
