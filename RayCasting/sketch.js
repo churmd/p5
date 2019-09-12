9ded7652b91720ffab84c321192b1cc249477983
churmd <churmdaniel@gmail.com>
2019-09-12 21:53:01 +0100

    Added controls for num rainbows and random walls. Made p5 scripts http links.

--- RayCasting/sketch.js
+++ RayCasting/sketch.js
@@ -1,6 +1,7 @@
 var light;
 var walls = [];
 var cnvWidth, cnvHeight;
+var numRainbowsSlider;
 var numRainbows = 0;
 
 function setup() {
@@ -10,6 +11,37 @@ function setup() {
   cnv.style('display', 'block');
 
   light = new LightSource(cnvWidth/2, cnvHeight/2);
+
+  createWalls();
+
+  createControls();
+}
+
+function draw() {
+  background(51);
+
+  if (mouseIsPressed) {
+    var inXBounds = mouseX > 0 && mouseX < cnvWidth;
+    var inYBounds = mouseY > 0 && mouseY < cnvHeight;
+    if (inXBounds && inYBounds) {
+      light.updatePosition(mouseX, mouseY);
+    }
+  }
+
+  walls.forEach(wall => {
+    wall.show(255);
+  });
+
+  numRainbows = numRainbowsSlider.value();
+  light.show(walls, numRainbows);
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function createWalls() {
+  walls = [];
 
   var topLeft = createVector(0, 0);
   var topRight = createVector(cnvWidth-1, 0);
@@ -28,29 +60,30 @@ function setup() {
     var y2 = random(cnvHeight);
     walls.push(new LineSegment(createVector(x1, y1), createVector(x2, y2)));
   }
-
-  input = createInput("Enter image URL");
-
 }
 
-function draw() {
-  background(51);
+function createControls() {
+  var controls = createDiv();
 
-  if (mouseIsPressed) {
-    var inXBounds = mouseX > 0 && mouseX < cnvWidth;
-    var inYBounds = mouseY > 0 && mouseY < cnvHeight;
-    if (inXBounds && inYBounds) {
-      light.updatePosition(mouseX, mouseY);
-    }
-  }
+  var label = createDiv('Number of Rainbows');
+  label.style('width', '70%');
+  label.style('display', 'inline-block');
+  label.style('text-align', 'center');
+  label.parent(controls);
 
-  walls.forEach(wall => {
-    wall.show(255);
-  });
+  var sliderDiv = createDiv();
+  sliderDiv.parent(label);
 
-  light.show(walls, numRainbows);
-}
+  numRainbowsSlider = createSlider(0, 20, numRainbows, 1);
+  numRainbowsSlider.size(cnvWidth/2);
+  numRainbowsSlider.parent(sliderDiv);
 
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
+  var buttonDiv = createDiv();
+  buttonDiv.style('width', '30%');
+  buttonDiv.style('display', 'inline-block');
+  buttonDiv.parent(controls);
+
+  var randomiseWalls = createButton('Randomise Walls');
+  randomiseWalls.parent(buttonDiv);
+  randomiseWalls.mousePressed(createWalls);
 }
