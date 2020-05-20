daf60d903bfd53af62090c1b4b0fcde6ff0a832f
churmd <churmdaniel@gmail.com>
2020-05-20 21:18:00 +0100

    SkecthTiles now link to animation page. Using SketchTiles for everything on home page.

--- src/components/sketchTile/SketchTile.jsx
+++ src/components/sketchTile/SketchTile.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useHistory } from "react-router-dom";
 import "./SketchTile.scss";
 
 function SketchTile(props) {
+	const history = useHistory();
+
+	function handleClick() {
+		history.push(props.routerPath);
+	}
+
 	return (
-		<div class='sketch-tile'>
+		<div class='sketch-tile' onClick={handleClick}>
 			<img src={props.image} alt='Avatar' class='image'></img>
 			<div class='overlay'>
 				<div class='text'>
@@ -20,6 +27,7 @@ SketchTile.propTypes = {
 	title: PropTypes.string.isRequired,
 	desc: PropTypes.string.isRequired,
 	image: PropTypes.string.isRequired,
+	routerPath: PropTypes.string.isRequired,
 };
 
 export default SketchTile;
---
+++ src/constants/RoutePaths.js
@@ -0,0 +1,12 @@
+export const home = "/"
+
+export const chaosGame = "/chaosGame" 
+export const flockingSimulation = "/flockingSimulation"
+export const lissajousCurveTable = "/lissajousCurveTable" 
+export const matrixRain = "/matrixRain"
+export const maurerRose = "/maurerRose"
+export const moduloTimesTable = "/moduloTimesTable"
+export const snowflakeGenerator = "/snowflakeGenerator" 
+ 
+
+
