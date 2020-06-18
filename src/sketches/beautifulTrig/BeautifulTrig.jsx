e79931eca1902b5288a4bd89c8fad0c01582d390
churmd <churmdaniel@gmail.com>
2020-06-18 20:06:11 +0100

    Improved render order, added outer circle, number of lines increases over time

--- src/sketches/beautifulTrig/BeautifulTrig.jsx
+++ src/sketches/beautifulTrig/BeautifulTrig.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import p5 from "p5";
-import ProjectedCosLine from "./ProjectedCosLine";
 
 class BeautifulTrig extends React.Component {
 	constructor(props) {
@@ -10,20 +9,12 @@ class BeautifulTrig extends React.Component {
 
 	sketch = (p) => {
 		let angle = 0;
-		let numLines = 16;
-		let lines = [];
+		let numLines = 1;
 		let stepSize = 0.01;
 
 		p.setup = () => {
 			const cnv = p.createCanvas(window.innerWidth, window.innerHeight);
 			cnv.style("display", "block");
-
-			let lineOffset = p.PI / numLines;
-			for (let i = 0; i < numLines; i++) {
-				let startingAngle = i * lineOffset;
-				let line = new ProjectedCosLine(p, startingAngle, stepSize);
-				lines.push(line);
-			}
 		};
 
 		p.draw = () => {
@@ -31,30 +22,84 @@ class BeautifulTrig extends React.Component {
 
 			let width = window.innerWidth;
 			let height = window.innerHeight;
-			let minSize = Math.min(width, height) * 0.9;
+			let lineLength = Math.min(width, height) * 0.9;
+			let halfLineLength = lineLength / 2;
 
 			p.translate(width / 2, height / 2);
 
+			showLines(halfLineLength);
+			showOuterCircleLine(lineLength);
+			showPoints(halfLineLength);
+			showOuterCirclePoint(halfLineLength);
+
+			angle += stepSize;
+			if (angle > p.TWO_PI) {
+				angle -= p.TWO_PI;
+				numLines += 1;
+				if (numLines > 32) {
+					numLines = 1;
+				}
+			}
+		};
+
+		let showLines = (halfLineLength) => {
 			// Rotate counter clockwise to match taking a catesian coordinate from the origin along the positve x axis
 			let roatationStep = -p.PI / numLines;
-			for (let i = 0; i < lines.length; i++) {
-				let line = lines[i];
-
+			for (let i = 0; i < numLines; i++) {
 				p.push();
+
 				p.rotate(i * roatationStep);
-				line.show(minSize);
-				line.update();
+				p.stroke(0);
+				p.strokeWeight(1);
+				p.line(-halfLineLength, 0, halfLineLength, 0);
+
 				p.pop();
 			}
+		};
 
-			let xPos = p.cos(angle) * (minSize / 2);
-			let yPos = p.sin(angle) * (minSize / 2);
+		let showOuterCircleLine = (diameter) => {
+			p.push();
 
-			p.strokeWeight(0);
+			p.stroke(0);
+			p.strokeWeight(5);
+			p.noFill();
+			p.ellipse(0, 0, diameter, diameter);
+
+			p.pop();
+		};
+
+		let showPoints = (halfLineLength) => {
+			// Rotate counter clockwise to match taking a catesian coordinate from the origin along the positve x axis
+			let roatationStep = -p.PI / numLines;
+			let pointOffset = p.PI / numLines;
+			for (let i = 0; i < numLines; i++) {
+				p.push();
+				p.rotate(i * roatationStep);
+
+				let pointAngle = angle + i * pointOffset;
+				let sinePosition = p.cos(pointAngle) * halfLineLength;
+
+				p.stroke(0);
+				p.strokeWeight(2);
+				p.fill(255);
+				p.ellipse(sinePosition, 0, 20, 20);
+
+				p.pop();
+			}
+		};
+
+		let showOuterCirclePoint = (halfLineLength) => {
+			let xPos = p.cos(angle) * halfLineLength;
+			let yPos = p.sin(angle) * halfLineLength;
+
+			p.push();
+
+			p.stroke(0);
+			p.strokeWeight(2);
 			p.fill(0, 0, 150);
 			p.ellipse(xPos, yPos, 20, 20);
 
-			angle += stepSize;
+			p.pop();
 		};
 
 		p.windowResized = () => {
