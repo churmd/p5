211226301333b008f0898be8c3ad5e247594fcee
Daniel Churm <churmdaniel@gmail.com>
2024-06-18 21:09:58 +0100

    Drops now renders as a series of vertices

--- src/sketches/inkMarbling/InkDrop.js
+++ src/sketches/inkMarbling/InkDrop.js
@@ -12,13 +12,29 @@ class InkDrop {
         this.p = p5Instance;
         this.center = this.p.createVector(centerX, centerY);
         this.r = radius;
+
+        this.vertices = [];
+        let numVertices = 100;
+        for (let index = 0; index < numVertices; index++) {
+            let angle = this.p.map(index, 0, numVertices, 0, this.p.TWO_PI);
+            let vertex = this.p.createVector(this.p.cos(angle), this.p.sin(angle));
+            vertex.mult(this.r);
+            vertex.add(this.center);
+            this.vertices.push(vertex);
+        }
     }
 
     show() {
         this.p.push();
         this.p.noStroke();
         this.p.fill(255);
-        this.p.circle(this.center.x, this.center.y, this.r)
+       
+        this.p.beginShape();
+        this.vertices.forEach((vertex) => {
+            this.p.vertex(vertex.x, vertex.y)
+        })
+        this.p.endShape(this.p.CLOSE)
+
         this.p.pop();
 
     }
